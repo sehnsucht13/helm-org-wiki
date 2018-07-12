aacbe95c95085048edba28f02272478aa135ea65
Yavor <ykonstantinov1@gmail.com>
2018-07-11 19:11:05 -0700

    Added some more source blocks. Created variables to hold the paths to the wiki.

--- helm-org-wiki.el
+++ helm-org-wiki.el
@@ -2,14 +2,38 @@
 ;;; Commentary:
 (require 'org)
 (require 'helm)
+(defun helm-org-wiki-rust-block ()
+  "Insert a Rust code block."
+(interactive)
+(insert "#+BEGIN_SRC rust")
+(org-return)
+(org-return)
+(insert "#+END_SRC")
+(previous-line 1))
+
+
+(defcustom helm-org-wiki-directory "~/Wiki"
+  "This variable contains the path to the main folder which houses the wiki."
+  :group 'helm-org-wiki
+  :type 'string)
+
+(defcustom helm-org-wiki-index "~/Wiki/index.org"
+  "Variable to hold the path to the wiki index"
+  :group 'helm-org-wiki
+  :type 'string)
 
 ;;; Code:
-(defun open-wiki-index ()
+(defun helm-org-wiki-open-index ()
 "Opens the wiki index."
 (interactive)
-(find-file "~/Wiki/index.org"))
+(find-file helm-org-wiki-index))
 
-(defun wiki-emacs-lisp-block ()
+(defun wiki-create-new-section ()
+  "Creates a new section into the wiki"
+  (interactive)
+  (helm-find-files helm-org-wiki-directory))
+
+(defun helm-org-wiki-emacs-lisp-block ()
   "Insert an Emacs-lisp block."
 (interactive)
 (insert "#+BEGIN_SRC emacs-lisp")
@@ -18,7 +42,7 @@
 (insert "#+END_SRC")
 (previous-line 1))
 
-(defun wiki-python-block ()
+(defun helm-org-wiki-python-block ()
   "Insert a Python code block."
 (interactive)
 (insert "#+BEGIN_SRC python")
@@ -27,7 +51,7 @@
 (insert "#+END_SRC")
 (previous-line 1))
 
-(defun wiki-latex-block ()
+(defun helm-org-wiki-latex-block ()
   "Insert a Latex code block."
 (interactive)
 (insert "#+BEGIN_SRC latex")
@@ -36,7 +60,7 @@
 (insert "#+END_SRC")
 (previous-line 1))
 
-(defun wiki-java-block ()
+(defun helm-org-wiki-java-block ()
   "Insert a Java code block."
 (interactive)
 (insert "#+BEGIN_SRC java")
@@ -45,8 +69,7 @@
 (insert "#+END_SRC")
 (previous-line 1))
 
-
-(defun wiki-javascript-block ()
+(defun helm-org-wiki-javascript-block ()
   "Insert a Javascript code block."
 (interactive)
 (insert "#+BEGIN_SRC js")
@@ -55,7 +78,7 @@
 (insert "#+END_SRC")
 (previous-line 1))
 
-(defun wiki-sh-block ()
+(defun helm-org-wiki-sh-block ()
   "Insert a Shell script code block."
 (interactive)
 (insert "#+BEGIN_SRC sh")
@@ -65,7 +88,7 @@
 (previous-line 1))
 
 
-(defun wiki-haskell-block ()
+(defun helm-org-wiki-haskell-block ()
   "Insert a Haskell code block."
 (interactive)
 (insert "#+BEGIN_SRC haskell")
@@ -75,7 +98,7 @@
 (previous-line 1))
 
 
-(defun wiki-C-block ()
+(defun helm-org-wiki-C-block ()
   "Insert a C code block."
 (interactive)
 (insert "#+BEGIN_SRC C")
@@ -84,8 +107,7 @@
 (insert "#+END_SRC")
 (previous-line 1))
 
-
-(defun wiki-C++-block ()
+(defun helm-org-wiki-C++-block ()
   "Insert a C++ code block."
 (interactive)
 (insert "#+BEGIN_SRC C++")
@@ -94,9 +116,18 @@
 (insert "#+END_SRC")
 (previous-line 1))
 
+(defun helm-org-wiki-lisp-block ()
+  "Insert a Common Lisp code block."
+(interactive)
+(insert "#+BEGIN_SRC lisp")
+(org-return)
+(org-return)
+(insert "#+END_SRC")
+(previous-line 1))
 
-(defun wiki-rust-block ()
-  "Insert a Rust code block."
+;;; Anything below here requires a plugin to be executed
+(defun helm-org-wiki-rust-block ()
+  "Insert a Rust code block. Requires the ob-rust plugin available from MELPA."
 (interactive)
 (insert "#+BEGIN_SRC rust")
 (org-return)
@@ -104,10 +135,19 @@
 (insert "#+END_SRC")
 (previous-line 1))
 
-(defun wiki-lisp-block ()
-  "Insert a Common Lisp code block."
+(defun helm-org-wiki-go-block ()
+  "Insert a Golang code block. Requires the ob-go plugin available from MELPA."
 (interactive)
-(insert "#+BEGIN_SRC lisp")
+(insert "#+BEGIN_SRC go")
+(org-return)
+(org-return)
+(insert "#+END_SRC")
+(previous-line 1))
+
+(defun helm-org-wiki-typescript-block ()
+  "Insert a Typescript code block. Requires the ob-typescript plugin available from MELPA."
+(interactive)
+(insert "#+BEGIN_SRC typescript")
 (org-return)
 (org-return)
 (insert "#+END_SRC")
