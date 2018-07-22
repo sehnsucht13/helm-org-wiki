0fe45f78d53ebdc49415655ca9238f1c2f2ae7f4
Yavor <ykonstantinov1@gmail.com>
2018-07-22 02:06:52 -0700

    Added Several functions to create new files within wiki. Added todo and readme.

--- helm-org-wiki.el
+++ helm-org-wiki.el
@@ -14,28 +14,49 @@
   :group 'helm-org-wiki
   :type 'string)
 
+(defun helm-org-wiki-create-new-wiki (WIKI-PATH)
+  "Create a new wiki directory along with an index."
+  (interactive "DEnter path to wiki: ")
+  (if (not (file-directory-p WIKI-PATH)) ;;Prevent overwriting a directory
+	  (progn
+		(make-directory WIKI-PATH)
+		(with-temp-buffer
+		  (insert "* Wiki Index")
+		  (write-file (concat WIKI-PATH "/Index.org"))))
+	(message "This directory already exists")))
+
 (defun helm-org-wiki-open-index ()
-  "Opens the wiki index."
+  "Open the wiki index."
   (interactive)
   (find-file helm-org-wiki-index))
 
-(defun helm-org-wiki--make-helm-src ()
+(defun helm-org-wiki--make-helm-src (DIR)
   "Create the sources for the wiki index."
-  (let ((DIR-LIST (directory-files helm-org-wiki-directory)))
-	(pop DIR-LIST)
-	(pop DIR-LIST)
+  (let ((DIR-LIST (directory-files DIR)))
 	DIR-LIST
 	))
 
-(defun wiki-create-new-section ()
+(defun helm-org-wiki-create-new-section ()
   "Create a new section into the wiki."
   (interactive)
   (helm :sources
 		(helm-build-sync-source "*New Wiki Section Source*"
-		  :candidates (helm-org-wiki--make-helm-src))
+		  :candidates (helm-org-wiki--make-helm-src helm-org-wiki-directory))
 		:buffer "*New Wiki Section*"))
 
+(defun helm-org-wiki-create-new-subsection ()
+  "Create a new subsection")
 
+(defun helm-org-wiki-rename-entry (NEW-NAME &rest save-on-rename)
+  "Rename the current Wiki entry to NEW-NAME.  If SAVE-ON-RENAME is true then the buffer is saved as well."
+  (interactive "sEnter a new file name ")
+  (rename-file (buffer-file-name) NEW-NAME)
+  (rename-buffer NEW-NAME)
+  (set-visited-file-name NEW-NAME)
+  (if save-on-rename
+	  (save-current-buffer)))
+
+;;; Code blocks below
 (defun helm-org-wiki-emacs-lisp-block ()
   "Insert an Emacs-lisp block."
   (interactive)
@@ -133,9 +154,9 @@
   (insert "#+END_SRC")
   (previous-line 1))
 
-;;; Anything below here requires a plugin to be executed
+;;; Anything below here requires a plugin to be evaluated
 (defun helm-org-wiki-rust-block ()
-  "Insert a Rust code block. Requires the ob-rust plugin available from MELPA."
+  "Insert a Rust code block.  Requires the ob-rust plugin available from MELPA."
   (interactive)
   (insert "#+BEGIN_SRC rust")
   (org-return)
@@ -144,7 +165,7 @@
   (previous-line 1))
 
 (defun helm-org-wiki-go-block ()
-  "Insert a Golang code block. Requires the ob-go plugin available from MELPA."
+  "Insert a Golang code block.  Requires the ob-go plugin available from MELPA."
   (interactive)
   (insert "#+BEGIN_SRC go")
   (org-return)
@@ -153,7 +174,7 @@
   (previous-line 1))
 
 (defun helm-org-wiki-typescript-block ()
-  "Insert a Typescript code block. Requires the ob-typescript plugin available from MELPA."
+  "Insert a Typescript code block.  Requires the ob-typescript plugin available from MELPA."
   (interactive)
   (insert "#+BEGIN_SRC typescript")
   (org-return)
